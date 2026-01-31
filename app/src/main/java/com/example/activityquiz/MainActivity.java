189782b5fa144792cc944784db722547e7630483
Marinay-ALTF4 <adriennemarinay@gmail.com>
2026-01-31 23:02:17 +0800

    Chapter 3: The Activity LifeCycle

--- app/src/main/java/com/example/activityquiz/MainActivity.java
+++ app/src/main/java/com/example/activityquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.activityquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -8,17 +9,16 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPreviousButton;
-
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -31,33 +31,48 @@ public class MainActivity extends AppCompatActivity {
     };
     private int mCurrentIndex = 0;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);  // optional
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
 
-        mQuestionTextView = findViewById(R.id.question_text_view);
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            Log.d(TAG, "Restored index: " + mCurrentIndex);
+        }
 
+        mQuestionTextView = findViewById(R.id.question_text_view);
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
         mNextButton = findViewById(R.id.next_button);
-        mPreviousButton = findViewById(R.id.previous_button);  // ← HERE
+        mPreviousButton = findViewById(R.id.previous_button);
 
         // True button click
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(true);
+                Log.d(TAG, "True button clicked at index " + mCurrentIndex);
+                checkAnswer(true, mTrueButton);
             }
         });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "False button clicked at index " + mCurrentIndex);
+                checkAnswer(false, mFalseButton);
+            }
+        });
+
 
         // False button click
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(false);
+                Log.d(TAG, "False button clicked at index " + mCurrentIndex);
+                checkAnswer(false, mFalseButton);
             }
         });
 
@@ -66,48 +81,94 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                Log.d(TAG, "Next button clicked. Index now: " + mCurrentIndex);
                 updateQuestion();
 
                 mTrueButton.setEnabled(true);
                 mFalseButton.setEnabled(true);
+
+                // Reset colors
+                mTrueButton.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+                mFalseButton.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
             }
         });
 
-        // ← Previous button click
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+                Log.d(TAG, "Previous button clicked. Index now: " + mCurrentIndex);
                 updateQuestion();
 
                 mTrueButton.setEnabled(true);
                 mFalseButton.setEnabled(true);
+
+                // Reset colors
+                mTrueButton.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+                mFalseButton.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
             }
         });
+
 
         // Show first question
         updateQuestion();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
-    private void checkAnswer(boolean userPressedTrue) {
+    private void checkAnswer(boolean userPressedTrue, Button clickedButton) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-        int messageResId = 0;
         if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+            clickedButton.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
         } else {
-            messageResId = R.string.incorrect_toast;
+            clickedButton.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-                .show();
+
+        // Disable both buttons after selection
+        mTrueButton.setEnabled(false);
+        mFalseButton.setEnabled(false);
     }
+
 }
-
-
